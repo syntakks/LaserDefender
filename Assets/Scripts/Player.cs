08d6d1e286b63305c1910abe05e872538bc027ce
Steve Wall <syntakks@gmail.com>
2020-01-24 16:48:46 -0600

    Initial Commit
    First Commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void CheckMove()
+    {
+        var deltaX = Input.GetAxis("Horizontal");
+        var newXPosition = transform.position.x + deltaX;
+        transform.position = new Vector2(newXPosition, transform.position.y); 
+    }
+}

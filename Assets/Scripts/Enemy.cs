94f6851703a5e8dab5a2a4a5bfbef846e3db9f51
Steve Wall <syntakks@gmail.com>
2020-01-27 10:28:21 -0600

    Added DamageDealer
    Added class to attach to any lasers, bombs, enemy. Things that deal damage. Added circle colliders to enemy prefabs.

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] float health = 100; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        CheckHit(other); 
+    }
+
+    private void CheckHit(Collider2D other)
+    {
+        DamageDealer damageDealer = other.gameObject.GetComponent<DamageDealer>();
+        if (damageDealer != null)
+        {
+            damageDealer.Hit();
+            health -= damageDealer.Damage;
+            CheckHealth(); 
+        }
+    }
+
+    private void CheckHealth()
+    {
+        if (health < 1)
+        {
+            Destroy(gameObject); 
+        }
+    }
+
+}
